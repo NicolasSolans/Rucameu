51e15f64c4be4b668299ea49569655caef4745be
Lucas Luppi <lucasgluppi@gmail.com>
2025-11-04 21:20:47 -0300

    corrección

--- src/Presentation/Controllers/SellPointController.cs
+++ src/Presentation/Controllers/SellPointController.cs
@@ -10,7 +10,7 @@ namespace Presentation.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    //[Authorize(Roles = "Admin, Employee")]
+    [Authorize(Roles = "Admin, Employee")]
     public class SellPointController : ControllerBase
     {
         private readonly ISellPointService _sellPointService;
